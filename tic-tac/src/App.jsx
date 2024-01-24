c40231ab8a5b94885be06d11a3fa1d48e814d7b6
divyendumaurya <rajatmaurya0720@gmail.com>
2024-01-24 17:19:08 +0530

    Initial commit

---
+++ tic-tac/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+import {Box} from "./components/Box"
+
+const App = () => {
+  return (
+    <>
+    
+    
+    <Box/>
+    </>
+  )
+}
+
+export default App
---
+++ tic-tac/src/components/Box.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Box = () => {
+  return (
+    <div>
+      box
+    </div>
+  )
+}
+
+export default Box
